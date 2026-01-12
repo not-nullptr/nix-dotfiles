842678229cd9ff0f46b15a03b312a5c316e8c2cc
not-nullptr <nullptr@vert.sh>
2026-01-12 23:12:39 +0000

    wait why am i building hyprland from scratch? lol

--- modules/global/desktop/hyprland.nix
+++ modules/global/desktop/hyprland.nix
@@ -1,25 +1,4 @@
-{ pkgs, ... }:
-
-let
-  commit = "5e181111216ba05c89b245d9b239b47a914fd714";
-in
 {
-  nixpkgs.overlays = [
-    (self: super: {
-      hyprland = super.hyprland.overrideAttrs (old: {
-        src = pkgs.fetchFromGitHub {
-          owner = "hyprwm";
-          repo = "Hyprland";
-          rev = commit;
-          # fetch submodules so subprojects/hyprland-protocols exists
-          fetchSubmodules = true;
-          # compute sha256 (see instructions below)
-          sha256 = "ZRzxg/4idOnm8AtiTN6S3fd+pTNmipfdVsNnAhRuwBc=";
-        };
-      });
-    })
-  ];
-
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
